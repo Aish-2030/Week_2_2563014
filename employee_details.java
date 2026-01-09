e9f8f209df257ff44da4244a80d2c793c315a0fc
Aishwarya Birajdar <anvibirajdar15@gmail.com>
2026-01-09 11:27:41 +0530

    Added department and salary to Employee instances

--- employee_details.java
+++ employee_details.java
@@ -31,16 +31,16 @@ public class employee_details
     public static void main(String[] args) 
     {
 
-        Employee e1 = new Employee("Anvi", 101);
-        Employee e2 = new Employee("Rohan", 102);
-        Employee e3 = new Employee("Sneha", 103);
-        Employee e4 = new Employee("Aditya", 104);
-        Employee e5 = new Employee("Om", 105);
-        Employee e6 = new Employee("Piyush", 106);
-        Employee e7 = new Employee("Ankita", 107);
-        Employee e8 = new Employee("Sanjana", 108);
-        Employee e9 = new Employee("Sanket", 109);
-        Employee e10 = new Employee("Vihan", 110);
+        Employee e1 = new Employee("Anvi", 101,"Finance",45000);
+        Employee e2 = new Employee("Rohan", 102,"IT",32000);
+        Employee e3 = new Employee("Sneha", 103,"HR",40000);
+        Employee e4 = new Employee("Aditya", 104,"Sales",35000);
+        Employee e5 = new Employee("Om", 105,"Marketing",39000);
+        Employee e6 = new Employee("Piyush", 106,"HR",41000);
+        Employee e7 = new Employee("Ankita", 107,"IT",47000);
+        Employee e8 = new Employee("Sanjana", 108,"Sales",30000);
+        Employee e9 = new Employee("Sanket", 109,"Support",31000);
+        Employee e10 = new Employee("Vihan", 110,"Finance",46000);
 
         Employee.showCompany();
         e1.showDetails();
@@ -57,5 +57,6 @@ public class employee_details
            
     }
 }
+
 
 
