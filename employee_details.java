7f3c01c6eabd5714f0358fe5dc118b35e3ae8dfc
Aishwarya Birajdar <anvibirajdar15@gmail.com>
2026-01-09 11:04:39 +0530

    Add files via upload
    Program to demonstrate static keyword and memory usage

---
+++ employee_details.java
@@ -0,0 +1,36 @@
+class Employee {
+
+    String name;                     // Each employee has their own name
+    int id;                          // Each employee has their own id
+
+    static String company = "TCS";   // One copy shared by ALL employees
+
+    Employee(String n, int i) {
+        name = n;
+        id = i;
+    }
+
+    void showDetails() {
+        System.out.println("Name: " + name + ", ID: " + id + ", Company: " + company);
+    }
+
+    static void showCompany() {
+        System.out.println("Company Name: " + company);
+    }
+}
+
+public class employee_details {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Anvi", 101);
+        Employee e2 = new Employee("Rohan", 102);
+        Employee e3 = new Employee("Sneha", 103);
+
+        Employee.showCompany();
+        e1.showDetails();
+        e2.showDetails();
+        e3.showDetails();
+
+           
+    }
+}
